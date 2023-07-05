3672160cd9a5c51892803c7a7a7ecf1dd38ca528
ParkSeongHo <psh55401@naver.com>
2023-07-05 16:15:57 +0900

    문자열 대소문자 바꿔서 출력 문제

---
+++ 대소문자 바꿔서 출력하기/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = "aBcDeFg";
+        String answer = "";
+
+        //Stack <Character> stack = new Stack <> ();
+        System.out.println(a.toCharArray());
+        for(Character c : a.toCharArray()){
+            if(Character.isUpperCase(c)){
+                //stack.push(Character.toLowerCase(c));
+                answer += Character.toLowerCase(c);
+            }
+            else if(Character.isLowerCase(c)){
+                //stack.push(Character.toUpperCase(c));
+                answer += Character.toUpperCase(c);
+            }
+        } 
+        System.out.println(answer);
+
+        sc.close();
+    }
+}
